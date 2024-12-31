b0387be9822a931a98f6a68257e14205ec06521f
Ayush Sahu <ayushsahu1430@gmail.com>
2024-12-31 07:58:33 +0530

    Captain Login and Logout done

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,40 +1,9 @@
 const captainModel = require('../models/captain.model'); 
 const captainService = require('../services/captain.service');
 const {validationResult} = require('express-validator');
+const blacklistedTokenModel = require('../models/blacklistToken.model');
 
-// module.exports.registerCaptain = async (req, res) => {
-//         const errors = validationResult(req);
-//         if(!errors.isEmpty()) {
-//             return res.status(400).json({errors: errors.array()});
-//         }
-
-//         const { firstname, lastname, email, password, vehicle } = req.body;
-
-//         const isCaptainExist = await captainModel.findOne({email});
-
-//         if(isCaptainExist) {
-//             return res.status(400).json({error: "Captain already exists"});
-//         }
-
-//         const hashedPassword = await captainService.hashPassword(password);
-
-//         const captain = await captainService.createCaptain({
-//             firstname,
-//             lastname,
-//             email,
-//             password: hashedPassword,
-//             color: vehicle.color,
-//             plate: vehicle.plate,
-//             capacity: vehicle.capacity,
-//             vehicleType: vehicle.vehicleType,
-//         });
-
-//         const token = captain.generateAuthToken();
-
-//         res.status(201).json({token, captain});
-// }
-
-module.exports.registerCaptain = async (req, res) => {
+module.exports.registerCaptain = async (req, res, next) => {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
@@ -71,5 +40,46 @@ module.exports.registerCaptain = async (req, res) => {
       console.error('Error:', error.message);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  };
+};
+
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password} = req.body;
+
+    const captain = await captainModel.findOne({email}).select('+password');
+
+    if(!captain) {
+        return res.status(400).json({error: "Invalid Email or Password"});
+    }
+
+    const isMatch = await captain.comparePassword(password);
+
+    if(!isMatch) {
+        return res.status(400).json({error: "Invalid Email or Password"});
+    }
+
+    const token = captain.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({token, captain});
+};
+
+module.exports.getCaptainProfile = async (req, res, next) => {
+    res.status(200).json({captain: req.captain});
+};
   
+module.exports.logoutCaptain = async (req, res, next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    await blacklistedTokenModel.create({token});
+    
+    res.clearCookie('token');
+    
+    res.status(200).json({message: "Logged out successfully"});
+};
--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blacklistedTokenModel = require('../models/blacklistToken.model');
+const captainModel = require('../models/captain.model');
 
 module.exports.authUser = async (req, res, next) => {
     // Log incoming headers
@@ -12,7 +14,7 @@ module.exports.authUser = async (req, res, next) => {
         return res.status(401).json({ message: "Unauthorized - Token missing" });
     }
 
-    const isBlacklisted = await userModel.findOne({token: token});
+    const isBlacklisted = await blacklistedTokenModel.findOne({token: token});
 
     if(isBlacklisted){
         return res.status(401).json({message: 'Unauthorized - Token blacklisted'});
@@ -31,5 +33,28 @@ module.exports.authUser = async (req, res, next) => {
     } catch (err) {
         console.error("Error in auth middleware: ", err);
         return res.status(401).json({ message: "Unauthorized - Token invalid" });
+    }
+};
+
+module.exports.authCaptain = async (req, res, next) => {
+    const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized - Token missing" });
+    }
+
+    const isBlacklisted = await blacklistedTokenModel.findOne({token: token});
+
+    if(isBlacklisted){
+        return res.status(401).json({message: 'Unauthorized - Token blacklisted'});
+    }
+
+    try{
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+        req.captain = captain;
+        return next();
+    } catch (err) {
+        res.status(401).json({message: 'Unauthorized - Token invalid'});
     }
 };
