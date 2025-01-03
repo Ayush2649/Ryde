5289d158a99b8d275aaa9ef8f85f7e0a571d7b5c
Ayush Sahu <ayushsahu1430@gmail.com>
2025-01-03 11:34:27 +0530

    Creating location search panel UI done

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -3,7 +3,46 @@ import React from 'react';
 const LocationSearchPanel = () => {
     return (
         <div>
-            Location Search Panel
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
+            <div className='gap-4 flex items-center my-4 justify-start'>
+                <h2 className='bg-[#eee] h-10 w-10 flex items-center justify-center rounded-full'><i className="ri-map-pin-fill"></i></h2>
+                <h4 className='text-lg font-semibold'>24B, Street 1, Block 1, Area 1, Dhaka, Bangladesh</h4>
+            </div>
         </div>
     )
 }
