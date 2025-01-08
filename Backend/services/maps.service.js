5752e1eec17ea8c2c720067665b7cc9ec8923cd8
Ayush Sahu <ayushsahu1430@gmail.com>
2025-01-08 16:59:11 +0530

    Creating suggestion , getCoordinates & distanceTime endpoint with google maps done

---
+++ Backend/services/maps.service.js
@@ -0,0 +1,75 @@
+const axios = require('axios');
+
+module.exports.getAddressCoordinate = async (address) => {
+    try {
+        const response = await axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+            params: {
+                address,
+                key: process.env.GOOGLE_MAPS_API, // Ensure you have a valid API key
+            },
+        });
+
+        if (response.data.status === 'ZERO_RESULTS') {
+            throw new Error('No results found for the provided address.');
+        } else if (response.data.status !== 'OK') {
+            throw new Error(`Geocoding API error: ${response.data.status}`);
+        }
+
+        const location = response.data.results[0].geometry.location;
+        return location;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+module.exports.getDistanceTime = async (origin, destination) => {
+    if(!origin || !destination) {
+        throw new Error('Origin and destination are required');
+    }
+
+    const APIKEY = process.env.GOOGLE_MAPS_API;
+
+    const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(origin)}&destinations=${encodeURIComponent(destination)}&key=${APIKEY}`;
+
+    try {
+
+
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+
+            if (response.data.rows[ 0 ].elements[ 0 ].status === 'ZERO_RESULTS') {
+                throw new Error('No routes found');
+            }
+
+            return response.data.rows[ 0 ].elements[ 0 ];
+        } else {
+            throw new Error('Unable to fetch distance and time');
+        }
+
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+module.exports.getAutoCompleteSuggestions = async (input) => {
+    if(!input) {
+        throw new Error('Input is required');
+    }
+
+    const APIKEY = process.env.GOOGLE_MAPS_API;
+
+    const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodeURIComponent(input)}&key=${APIKEY}`;
+
+    try {
+        const response = await axios.get(url);
+        if (response.data.status === 'OK') {
+            return response.data.predictions;
+        } else {
+            throw new Error('Unable to fetch suggestions');
+        }
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
